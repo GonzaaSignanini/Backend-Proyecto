e4866c63917f9de51e3a458d44f9791662ab035e
Gonzalo Signanini <gonzasignanini@hotmail.com>
2022-05-29 21:49:53 -0300

    Proyecto Final - Falta Algunas Correcciones

--- proyectoFinal/src/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import usersController from '../controllers/users.controller.js'
-
-router.get('/:pid', usersController.getUserById)
-router.get('/cart/:cid', usersController.getUserByCart)
-
-export default router
--- proyectoFinal/src/services/productsService.js
+++
@@ -1,8 +0,0 @@
-import Products from "../model/Products.js"
-import GenericQueries from "./genericQueries.js"
-
-export default class ProductsService extends GenericQueries{
-    constructor(dao){
-        super(dao,Products.model)
-    }
-}
